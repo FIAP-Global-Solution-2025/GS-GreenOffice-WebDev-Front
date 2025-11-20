ae02e81d4e40d9841d24305a5d879e76dc6d5c91
MathBra17 <matheusbrasilangelotti@gmail.com>
2025-11-20 09:43:23 -0300

    Components e Pages Criados

--- greenoffice/src/App.jsx
+++ greenoffice/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Contribuidores from "./pages/Contribuidores";
+import Login from "./pages/Login";
+import Mensagens from "./pages/Mensagens";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+  const handlerLogout = () => setUser(null);
+
+  const handlerLogin = (userData) => {
+    setUser(userData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div
+        className={`min-h-screen transition-colors duration-300 ${darkMode ? "bg-gray-900 text-white" : "bg-gray-50 text-gray-900"
+          }`}
+      >
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} user={user} onLogout={handlerLogout} />
+
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home darkMode={darkMode} />} />
+            <Route
+              path="/contribuidores"
+              element={<Contribuidores darkMode={darkMode} user={user}/>}
+            />
+            <Route
+              path="/login"
+              element={<Login darkMode={darkMode} onLogin={handlerLogin} />}
+            />
+            <Route
+              path="/mensagens"
+              element={<Mensagens darkMode={darkMode} onLogin={handlerLogin} />}
+            />
+          </Routes>
+        </main>
+
+        <Footer darkMode={darkMode} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
